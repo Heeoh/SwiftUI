09e39680e522855c8bef01fb79268d94adce9a1e
heeoh <hio4662@naver.com>
2023-05-09 18:49:22 +0900

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Heeoh Son on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
